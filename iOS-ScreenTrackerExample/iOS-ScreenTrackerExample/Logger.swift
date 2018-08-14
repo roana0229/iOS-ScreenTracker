cc2d393977203f37c4773d867c59665e3962eb7b
roana0229 <roana.enter@gmail.com>
2018-08-14 18:46:08 +0900

    update: サンプルの更新

--- iOS-ScreenTrackerExample/iOS-ScreenTrackerExample/Logger.swift
+++ iOS-ScreenTrackerExample/iOS-ScreenTrackerExample/Logger.swift
@@ -3,21 +3,21 @@ import UIKit
 class Logger {
 
     class func log(_ function: String = #function, file: String = #file, line: Int = #line) {
-//        #if DEBUG
-//        let filename = file.components(separatedBy: "/").last!
-//        print("\(filename):L\(line):\(function)")
-//        #endif
+        #if DEBUG
+        let filename = file.components(separatedBy: "/").last!
+        print("\(filename):L\(line):\(function)")
+        #endif
     }
 
     class func log(_ message: Any?, function: String = #function, file: String = #file, line: Int = #line) {
-//        #if DEBUG
-//        let filename = file.components(separatedBy: "/").last!
-//        if message == nil {
-//            print("\(filename):L\(line):\(function)")
-//        } else {
-//            print("\(filename):L\(line):\(function) \"\(message!)\"")
-//        }
-//        #endif
+        #if DEBUG
+        let filename = file.components(separatedBy: "/").last!
+        if message == nil {
+            print("\(filename):L\(line):\(function)")
+        } else {
+            print("\(filename):L\(line):\(function) \"\(message!)\"")
+        }
+        #endif
     }
 
 }
